fda1754623069ad1d360091ab646ab010e674f92
Kostya Oliinyk <aquila0905@gmail.com>
2025-04-15 22:48:41 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const largeImg = document.getElementById('largeImg');
+  const thumbs = document.getElementById('thumbs');
+
+  thumbs.addEventListener('click', (e) => {
+    e.preventDefault(); // Забороняємо перехід по посиланню
+
+    const target = e.target;
+
+    if (target.tagName === 'IMG' && target.closest('a')) {
+      const link = target.closest('a');
+      const newSrc = link.getAttribute('href');
+
+      largeImg.setAttribute('src', newSrc);
+      largeImg.setAttribute('alt', link.getAttribute('title'));
+    }
+  });
+});
